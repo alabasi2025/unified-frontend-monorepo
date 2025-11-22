0fd520f6400062517357dc30871421a6425c4723
alabasi2025 <238506735+alabasi2025@users.noreply.github.com>
2025-11-22 18:30:55 -0500

    fix: Add missing environment files

---
+++ apps/platform-shell-ui/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://72.61.111.217'
+};
---
+++ apps/platform-shell-ui/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://72.61.111.217'
+};
